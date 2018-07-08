e3e3e6d15364251f800ab171695ab4133e5c446a
Talbene <talbene@gmail.com>
2018-07-08 14:27:11 +0300

    Updates

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import {reducers} from '../reducers/reducers'
-
-export function configureStore(initialState = {}) {
-  const store = createStore(reducers, initialState);
-  return store;
-}
-
-export const store = configureStore();
--- src/types/types.js
+++
@@ -1,3 +0,0 @@
-export const ACTIVATE_GEOD = 'ACTIVATE_GEOD'
-
-export const CLOSE_GEOD = 'CLOSE_GEOD'
